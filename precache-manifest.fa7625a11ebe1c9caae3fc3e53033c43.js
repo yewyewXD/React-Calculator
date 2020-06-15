2e3a437ada2d56771687b3963602adb2c632c89c
Yew Kang Wei <yewyew6933@gmail.com>
2020-06-15 19:20:04 +0800

    Updates

---
+++ precache-manifest.fa7625a11ebe1c9caae3fc3e53033c43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b6f5d5f7ba9ffa914408950378c6731c",
+    "url": "/React-Calculator/index.html"
+  },
+  {
+    "revision": "3ee1b01a4a850f228739",
+    "url": "/React-Calculator/static/css/main.09bf88e6.chunk.css"
+  },
+  {
+    "revision": "80f96290f4e2734747b8",
+    "url": "/React-Calculator/static/js/2.c0d94b55.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Calculator/static/js/2.c0d94b55.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3ee1b01a4a850f228739",
+    "url": "/React-Calculator/static/js/main.9b5bc9eb.chunk.js"
+  },
+  {
+    "revision": "a044a8aa7ab932419ded",
+    "url": "/React-Calculator/static/js/runtime-main.4588dd56.js"
+  }
+]);
